c3f208f62521c0ee44d2b8c812d497f54152e1b4
Siddharth Kundu <sk31395@gmail.com>
2018-10-07 23:43:40 +0530

    Deleting data from firestore database

--- app.js
+++ app.js
@@ -6,15 +6,28 @@ function renderCafe(doc) {
   let li = document.createElement("li");
   let name = document.createElement("span");
   let city = document.createElement("span");
+  let cross = document.createElement("div");
 
   li.setAttribute("data-id", doc.id);
   name.textContent = doc.data().name;
   city.textContent = doc.data().city;
+  cross.textContent = "x";
 
   li.appendChild(name);
   li.appendChild(city);
+  li.appendChild(cross);
 
   cafeList.appendChild(li);
+
+  // deleting data
+  cross.addEventListener("click", e => {
+    e.stopPropagation();
+    let id = e.target.parentElement.getAttribute("data-id");
+    // using .doc() to find single document and delete() to delete the document
+    db.collection("cafes")
+      .doc(id)
+      .delete();
+  });
 }
 
 // Getting data
